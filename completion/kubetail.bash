8bfd79960dc6475fbc86cd50b5c5f18ba6be9d4b
Vlad Bulimac <vbulimac@deloittece.com>
2020-06-03 15:08:53 +0300

    fixes --pods autocomplete and adds --label argument which can find pods by labels

--- completion/kubetail.bash
+++ completion/kubetail.bash
@@ -1,38 +1,34 @@
 #/usr/bin/env bash
 
-_findpods(){
-    local next="0"
-    local pods="";
-    for wo in "${COMP_WORDS[@]}"
-    do
-        if [ "$next" = "0" ]; then
-            if [ "$wo" = "-t" ] || [ "$wo" = "--pods" ]; then
-                next="1"
-            fi
-        else
-            pods="$wo"
-            break
-        fi
-    done
-    if [ "$pods" != "" ]; then
-        printf "%s" " --pods $pods"
-    fi
-}
-
-_kubetail()
-{
+_kubetail(){
     local curr_arg;
     curr_arg=${COMP_WORDS[COMP_CWORD]}
     prev=${COMP_WORDS[COMP_CWORD-1]}
     command=${COMP_WORDS[1]}
-    case $prev in
-        -p*| --pods*)
-            COMPREPLY=( $(compgen -W "$(kubectl get pods | awk '{print $1}')" -- $curr_arg ) );
-        ;;
-        # *)
-        #     COMPREPLY=( $(compgen -W "--pods --tail --grep" -- $curr_arg ) );
-        # ;;
-    esac
+
+    for (( idx=${#COMP_WORDS[@]}-1 ; idx>=0 ; idx-- )) ; do
+        if [[ ${COMP_WORDS[idx]} == --* ]]; then
+            last_param=${COMP_WORDS[idx]}
+            break
+        fi
+    done
+
+    if [[ "${last_param}" == "--pods" ]]; then
+        arg=$(kubectl get pods | awk '{print $1}')
+        arg="$(kubectl get pods | grep -v "NAME" | awk '{print $1}')"
+        COMPREPLY=( $(compgen -W "$arg" -- $curr_arg ) );
+    else
+        COMPREPLY=( $(compgen -W "--pods --tail --grep" -- $curr_arg ) );
+    fi
+
+    # case $prev in
+    #     -p*| --pods*)
+    #         COMPREPLY=( $(compgen -W "$(kubectl get pods | awk '{print $1}')" -- $curr_arg ) );
+    #     ;;
+    #     *)
+    #         COMPREPLY=( $(compgen -W "--pods --tail --grep" -- $curr_arg ) );
+    #     ;;
+    # esac
 }
 
 complete -F _kubetail kubetail kt
